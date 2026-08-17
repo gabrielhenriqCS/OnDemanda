da53be6420c0c1ba4bd1ccd8f5b0b805cf6c2e00
Gabriel Henrique <henriquedeveloper24@gmail.com>
2026-08-17 15:34:12 -0300

    style: mudando o formato da tela de Login

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,9 +2,9 @@ import NavBar from './components/NavBar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className='flex items-center justify-center h-[800px]'>
+        <body className='min-h-screen'>
             <NavBar />
             <main className="p-4">{children}</main>
-        </div>
+        </body>
     )
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -25,58 +25,67 @@ export default function Login() {
     }
   };
   return (
-    <div className="p-8 rounded-2xl shadow-sm border border-zinc-300 w-full max-w-md bg-blend-luminosity">
-      <div>
+    <div className="flex flex-col h-[800px] items-center justify-center bg-blend-luminosity p-8 max-w-2xl mx-auto">
+      <div className="mb-8 text-center">
         <h1 className="text-2xl font-bold text-zinc-800">OnComanda</h1>
         <p className="text-zinc-700 text-sm mt-1">Acessar o sistema de gestão</p>
       </div>
       <form
-        className="space-y-4"
+        className="space-y-4 flex flex-col px-22 py-30 bg-blue-300 rounded-md"
         onSubmit={handleSubmit}
       >
-        <label htmlFor="email" className="text-[21px] my-2">
+        <div>
+          <label htmlFor="email" className="block text-lg font-medium text-zinc-800 mb-1">
           E-mail
         </label>
         <input
           type="email"
-          id="email"
-          className="px-3 py-2 w-sm bg-white rounded-md"
+            id="email"
+            placeholder="email@exemplo.com"
+            className="px-3 py-2 w-sm bg-white rounded-md border border-gray-300
+          focus:outline-none focus:ring-1 focus:ring-zinc-500"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
 
-        <label htmlFor="senha" className="text-[21px] my-2">
+        <div>
+          <label htmlFor="senha" className="block text-lg font-medium text-zinc-800 mb-1">
           Senha
         </label>
         <input
           type="password"
-          id="senha"
-          className="px-3 w-sm py-2 rounded-md bg-white"
+            id="senha"
+            placeholder="********"
+          className="px-3 w-sm py-2 rounded-md bg-white border-gray-300
+          focus:outline-none focus:ring-1 focus:ring-zinc-500"
           value={senha}
           onChange={(e) => setSenha(e.target.value)}
         />
-        <div className="flex flex-row items-center mt-2 gap-2">
+        </div>
+        <div className="flex items-center">
           <input
             type="checkbox"
             id="lembrarSenha"
             checked={lembrarSenha}
             onChange={(e) => setLembrarSenha(e.target.checked)}
-            className="w-4 h-4 accent-blue-500 border-5"
+            className="w-4 h-4 rounded border-zinc-300 text-zinc-900 focus:ring-zinc-500"
           />
-          <p className="text-[16px] font-semibold">Lembrar-me</p>
+          <p className="ml-2 block text-sm text-zinc-500">Lembrar-me</p>
         </div>
 
-        <button
-          type="submit"
-          className="mt-10 py-2 px-0.5 bg-blue-700 text-white rounded-md hover:bg-blue-500 cursor-pointer"
-        >
-          Entrar
-        </button>
         {erro && (
-          <p className="bg-red-500 text-white p-2 rounded-md mt-3 text-center">
+          <p className="hidden text-red-600 bg-red-200 text-sm py-2">
             {erro}
           </p>
         )}
+        <button
+          type="submit"
+          className="w-full font-medium  py-2 px-4 bg-blue-700 text-white rounded-md hover:bg-blue-500 cursor-pointer"
+        >
+          Entrar
+        </button>
+        
       </form>
     </div>
   );
